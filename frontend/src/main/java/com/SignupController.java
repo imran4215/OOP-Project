e625ed5fb6ce5a406abee7e97123b0047cbe1a70
imran4215 <ahmedalimran83@gmail.com>
2025-02-11 08:01:08 +0600

    Add login and signup

---
+++ frontend/src/main/java/com/SignupController.java
@@ -0,0 +1,13 @@
+package com;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+
+public class SignupController {
+
+    @FXML
+    private void switchToLogin() throws IOException {
+        App.setRoot("login");
+    }
+}
